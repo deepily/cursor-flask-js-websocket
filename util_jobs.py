fc58b01ca8ec2a4196e517f8bcc1a7f152ab549c
Deepily <42214371+deepily@users.noreply.github.com>
2023-08-31 14:26:52 -0400

    Push get_current_time() into this utility file

---
+++ util_jobs.py
@@ -0,0 +1,13 @@
+import datetime as dt
+
+"""
+Get current date time, formatted as a string
+"""
+def get_current_datetime():
+    
+    now = dt.datetime.now()
+    return now.strftime("%m-%d-%Y @ %H:%M:%S")
+
+if __name__ == "__main__":
+
+    print("Current date and time: ", get_current_datetime() )

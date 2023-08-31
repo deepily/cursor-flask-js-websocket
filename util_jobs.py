45593d272cc8dcee3938a44f4082bd8805b43601
Deepily <42214371+deepily@users.noreply.github.com>
2023-08-31 14:46:08 -0400

    Add get_date()

--- util_jobs.py
+++ util_jobs.py
@@ -8,6 +8,14 @@ def get_current_datetime():
     now = dt.datetime.now()
     return now.strftime("%m-%d-%Y @ %H:%M:%S")
 
+"""
+Get current date, formatted as a string
+"""
+def get_current_date():
+    
+    now = dt.datetime.now()
+    return now.strftime("%m-%d-%Y")
+
 if __name__ == "__main__":
 
     print("Current date and time: ", get_current_datetime() )
